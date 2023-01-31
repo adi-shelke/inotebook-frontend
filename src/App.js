3e2ea80d8c5d3579571a2176ace00edec4183d67
Adinath Shelke <shelkeadinath3@gmail.com>
2023-01-31 23:23:35 +0530

    built the about us page and hosted web

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import NoteState from "./context/notes/NoteState";
 import { Alert } from "./components/Alert";
 import { Login } from "./components/Login";
 import { SignUp } from "./components/SignUp";
-
 function App() {
   return (
+    // style={{marginLeft:"0px",marginRight:"0px",maxWidth:"100vw",paddingRight:"0px",paddingLeft:"0px"}}
     <div>
     <NoteState>
       <BrowserRouter>
         <Navbar />
         <Alert message="this is alert"/>
-        <div className="container h-100">
+        <div className="container h-100" >
         <Routes>
           {/* <Route path="/*" element={<Login />} /> */}
           <Route path="/*" element={<Home />} />
